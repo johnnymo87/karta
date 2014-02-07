d94ef496f6658f433417131f6b7b5d04c091c490
johnnymo87 <johnnymo87@gmail.com>
2014-02-07 03:12:30 +0000

    init

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+ruby '1.9.3'
+gem 'rails', '3.2.14'
+gem 'pg'
+
+group :development do
+  gem 'debugger', '1.6.1'
+end
+
+gem 'gmaps4rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karta::Application.config.secret_token = '477bd2f33cd1a092edbbe60105e070024bbb5253271ebeb0e9bfc9cfafeeb2e07c1c8a34cffd07d14371efd8ee166c9449eb23719579f978c493f5af59ecf266'
